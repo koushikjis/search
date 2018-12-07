88f6765370f7150e720900b5710a18145d4a20c7
Koushik Roy <koushikroy.jis@gmail.com>
2018-12-07 10:56:20 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBox from "./SearchBar";
+
+function App() {
+  return (
+    <div className="ui container" style={{ margin: "15px" }}>
+      <SearchBox />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+class SearchBox extends React.Component {
+  constructor() {
+    super();
+    this.state = { term: "" };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
