02e72bad38c6178772c83c41e8e07c97df4d5679
Koushik Roy <koushikroy.jis@gmail.com>
2018-12-11 02:50:51 +0530

    alt

--- src/components/imageList.jsx
+++ src/components/imageList.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function imageList(props) {
   const imageList = props.images.map(image => {
-    return <img src={image.urls.regular} key={image.id} width="500px" />;
+    return (
+      <img
+        src={image.urls.regular}
+        key={image.id}
+        width="200px"
+        alt={image.description}
+      />
+    );
   });
   return <div>{imageList}</div>;
 }
